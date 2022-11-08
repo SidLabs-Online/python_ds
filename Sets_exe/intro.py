6cb10d82a22e807bf7b93f60542ec1b7e8ae7c0a
SidLabs-Online <sidlabs.online@gmail.com>
2022-11-08 18:13:09 +0530

    nov 8th updates

---
+++ Sets_exe/intro.py
@@ -0,0 +1,12 @@
+def intro_sets():
+  var1 = {"sid", "labs", "online"}
+  print("var1's data type is :  ", type(var1))
+  print("************")
+  print("Type casting a list into a set")
+  myList = ["s", "i", "d", "l", "a", "b", "s"]
+  print(type(myList))
+  newSet = set(myList)
+  print(type(newSet))
+  print("************")
+  
+
---
+++ dictionaries_exe/challenge_2.py
@@ -0,0 +1,13 @@
+# use the get() method to print the values for 
+# model & year key's from the given dict 
+
+carDict = {
+  "brand" : "Ford",
+  "model" : "Mustang",
+  "year" : 1990,
+}
+
+
+def challenge_2():
+  # write your solution here!
+  print()
