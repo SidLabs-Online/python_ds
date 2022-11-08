099e1dfb44e2a98026215960a17fc63c9dd1a24d
SidLabs-Online <sidlabs.online@gmail.com>
2022-11-08 18:35:10 +0530

    nov 8 updates

--- dictionaries_exe/challenge_2.py
+++ dictionaries_exe/challenge_2.py
@@ -1,6 +1,10 @@
 # use the get() method to print the values for 
 # model & year key's from the given dict 
 
+# Challenge 3 
+# Find out if a value i.e. 1999 exist 
+# in a given dictionary
+
 carDict = {
   "brand" : "Ford",
   "model" : "Mustang",
@@ -10,4 +14,12 @@ carDict = {
 
 def challenge_2():
   # write your solution here!
-  print()
+  print("Solution challenge 2: \n")
+  print(carDict.get("model"))
+
+def challenge_3():
+
+  # write your solution here!
+  print("Solution challenge 2: \n")
+
+  
--- dictionaries_exe/main.py
+++ dictionaries_exe/main.py
@@ -2,15 +2,15 @@ import intro_dict as intro
 import challenge as cg
 import  challenge_2 as cg2
 
-print("\n******** Dict Intro ********\n")
-intro.dict_intro()
-print("\n******** Dict Loops ********\n")
-intro.loop_dict()
-print("\n******** Dict Methods ********\n")
-intro.dict_methods()
+# print("\n******** Dict Intro ********\n")
+# intro.dict_intro()
+# print("\n******** Dict Loops ********\n")
+# intro.loop_dict()
+# print("\n******** Dict Methods ********\n")
+# intro.dict_methods()
 
-print("\n******** Dict Challenge ********\n")
-cg.driver()
+# print("\n******** Dict Challenge ********\n")
+# cg.driver()
 
 print("\n******** Challenge 2 ********\n")
 cg2.challenge_2()
